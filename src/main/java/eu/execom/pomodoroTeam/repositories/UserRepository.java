7e3ba04334c48e562d269702a89d5bded1ad0ac5
Minja Slijepcevic <minja.slp@gmail.com>
2018-08-21 09:30:56 +0200

    Adding scope profile email in app yml and final formatting clean up

--- src/main/java/eu/execom/pomodoroTeam/repositories/UserRepository.java
+++ src/main/java/eu/execom/pomodoroTeam/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package eu.execom.pomodoroTeam.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import eu.execom.pomodoroTeam.entities.UserEntity;
 
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
-	UserEntity findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
 }
