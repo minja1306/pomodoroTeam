34dccef7d37e975d39cf15aca5e7ca4c3773c59e
Bojana Ulic <ulicbojana@yahoo.com>
2018-08-31 14:27:28 +0200

    Adding team controller , sevices, team entity and additons to user, aditional changes

--- src/main/java/eu/execom/pomodoroTeam/entities/TeamEntity.java
+++ src/main/java/eu/execom/pomodoroTeam/entities/TeamEntity.java
@@ -4,28 +4,37 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.ToString;
 
 @Data
 @Entity
 @NoArgsConstructor
+@ToString(exclude = {"user"})
 public class TeamEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @ManyToMany(cascade = CascadeType.REFRESH)
-    @JoinTable(name = "User_Team", joinColumns = {
-            @JoinColumn(name = "Team_id", nullable = false, updatable = false)}, inverseJoinColumns = {
-            @JoinColumn(name = "User_id", nullable = false, updatable = false)})
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "User_Team", joinColumns = @JoinColumn(name = "Team_id"), inverseJoinColumns = @JoinColumn(name = "User_id"))
     private List<UserEntity> user = new ArrayList<>();
+
 }
--- src/main/java/eu/execom/pomodoroTeam/services/TeamService.java
+++ src/main/java/eu/execom/pomodoroTeam/services/TeamService.java
@@ -1,17 +1,7 @@
 package eu.execom.pomodoroTeam.services;
-
-import eu.execom.pomodoroTeam.entities.TeamEntity;
-import eu.execom.pomodoroTeam.repositories.TeamRepository;
-import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
-
-
-
-
 public class TeamService {
-
-
 
 }
